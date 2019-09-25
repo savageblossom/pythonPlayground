c24e2023b73c1c67ed5d87f7eeb4c9b23da22dcf
savageblossom <umer.muradasiloff@gmail.com>
2019-09-25 23:10:35 +0300

    updated, almost done lab3.py

---
+++ lab3.py
@@ -0,0 +1,19 @@
+# # Task 1
+# print('Enter number a: ')
+# a = int(input())
+# print('Enter number b: ')
+# b = int(input())
+# print('Enter operation (+, -, *, /): ')
+# op = input()
+#
+# if(op == "+") : print(a + b)
+# elif(op == "-") : print(a - b)
+# elif(op == "*") : print(a * b)
+# elif(op == "/") :
+#     if(a != 0) : print(a / b)
+#     else: print('Error. Division by zero!')
+
+# Task 2
+print('Enter your expression (i.e. 13+37): ')
+result = input()
+print(result)
